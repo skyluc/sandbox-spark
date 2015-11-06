7581006b8c679f3dc3704e43ba1a1b6632066ef7
Luc Bourlier <luc.bourlier@typesafe.com>
2015-11-06 12:46:44 +0000

    Update the test app to be used with spark-submit in cluster mode

--- test-app/build.sbt
+++ test-app/build.sbt
@@ -2,7 +2,8 @@ lazy val root = (project in file(".")).
   settings(
     name := "test-app-spark",
     version := "0.1",
-    scalaVersion := "2.11.4",
+    scalaVersion := "2.11.7",
     resolvers += "bintray-typesafe-maven-releases" at "http://dl.bintray.com/typesafe/maven-releases",
-    libraryDependencies += "org.apache.spark" %% "spark-core" % "1.3.1-typesafe-002"
+    resolvers += "spark rc" at "https://repository.apache.org/content/repositories/orgapachespark-1152",
+    libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2" % "provided"
   )
--- test-app/src/main/scala/org/example/spark/test/Test.scala
+++ test-app/src/main/scala/org/example/spark/test/Test.scala
@@ -7,14 +7,13 @@ import org.apache.spark.SparkConf
 object Test {
 
   val appName = "Luc-test"
-  val master = "mesos://master:5050"
 
   def main(args: Array[String]) {
     val conf = new SparkConf()
                      .setAppName(appName)
-                     .setMaster(master)
+//                     .setMaster(master)
                      .set("spark.mesos.executor.home", "bahh") // just has to be set ...
-                     .set("spark.executor.uri", "http://s3.amazonaws.com/spark-test-luc/releases/spark-1.3.1-typesafe-002-bin-2.4.0.tgz")
+                     .set("spark.executor.uri", "hdfs://10.10.1.15:8020/var/spark/spark-1.5.2-bin-hadoop2.6.tgz")
     val sc = new SparkContext(conf)
 
     val rdd = sc.makeRDD(2 to 5)
